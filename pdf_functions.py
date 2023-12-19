3ff353d6481e46d5a7821bec707c2049f66f53e2
Aiswarya S <134680109+aishushan@users.noreply.github.com>
2023-12-19 21:13:07 +0530

    Update pdf_functions.py

--- pdf_functions.py
+++ pdf_functions.py
@@ -1,30 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 19 20:10:52 2023
-
-@author: Aiswarya
-"""
-
 # pdf_functions.py
-import fitz  # PyMuPDF
+import fitz
 from rake_nltk import Rake
 import re
+from io import BytesIO
 
-def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page_num in range(doc.page_count):
-        page = doc[page_num]
-        text += page.get_text("text.utf8").replace('\uFFFD', 't')
+class PDFFunctions:
+    @staticmethod
+    def extract_text_from_pdf(pdf_content):
+        doc = fitz.open(stream=pdf_content)
+        text = ""
+        for page_num in range(doc.page_count):
+            page = doc[page_num]
+            text += page.get_text("text.utf8").replace('\uFFFD', 't')
+        return text
 
-    return text
+    @staticmethod
+    def extract_keywords_from_text(text):
+        r = Rake()
+        r.extract_keywords_from_text(text)
+        keywords = r.get_ranked_phrases_with_scores()
+        return keywords
 
-def extract_keywords_from_text(text):
-    r = Rake()
-    r.extract_keywords_from_text(text)
-    keywords = r.get_ranked_phrases_with_scores()
-    return keywords
-
-def clean_keywords(keywords):
-    cleaned_keywords = [(score, re.sub(r'[^a-zA-Z0-9\s]', '', phrase)) for score, phrase in keywords]
-    return cleaned_keywords
+    @staticmethod
+    def clean_keywords(keywords):
+        cleaned_keywords = [(score, re.sub(r'[^a-zA-Z0-9\s]', '', phrase)) for score, phrase in keywords]
+        return cleaned_keywords
